d10ce00b416ad57d61d1080f67ac439fe9e77a17
Youk Chang SU <csyouk@hanmail.net>
2016-11-24 15:01:21 +0900

    Initial Commit

---
+++ cpp-scratch/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cpp-scratch
+//
+//  Created by Nolgong on 24/11/2016.
+//  Copyright © 2016 csyouk. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
